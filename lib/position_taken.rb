af787d1c870d0b0fd39d6c1135638e811bc76ac4
Jaime Rogstad <jaimerogstad@gmail.com>
2020-07-13 16:02:48 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,5 +5,3 @@ def position_taken?(board, index)
     "true"
   end
 end
-
-# Address the "edge cases" - scenarios that qualify as an empty space that isn't equal to the literal " ". For example, we would want to consider "" to be an empty space as well.
