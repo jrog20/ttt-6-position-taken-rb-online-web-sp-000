3d5e05742d595f5d9b7ef38e54f1cbd8bce83717
Jaime Rogstad <jaimerogstad@gmail.com>
2020-07-13 15:53:24 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,13 @@
 # #position_taken? method
 
 def position_taken?(board, index)
-  if board[index] == " "
+  if board[index] == X || O
+    "true"
+  elsif board[index] == " "
+    "false"
+  elsif board[index] == ""
+    "false"
+  else
     "false"
   end
 end
