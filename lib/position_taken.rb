d6a9920a8eaffab498a97f9eaf006afd43f51a10
Jaime Rogstad <jaimerogstad@gmail.com>
2020-07-13 16:08:16 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,8 +3,7 @@
 def position_taken?(board, index)
   if board[index] == "X" || "O"
     true
-  else
-  #else board[index] == " " || "" || nil
+  else board[index] == " " || "" || nil
     false
   end  
 end
